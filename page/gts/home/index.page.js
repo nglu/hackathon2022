2f1ed55350b5f9ced1db23b2526faae63aa4bfa9
Ross Nikolai Montepalco <montepalco.ross@gmail.com>
2022-10-20 12:14:48 -0700

    Testing text widgets

--- page/gts/home/index.page.js
+++ page/gts/home/index.page.js
@@ -1,6 +1,7 @@
 import * as hmUI from '@zos/ui'
 import { log as Logger } from '@zos/utils'
 import { TEXT_STYLE } from './index.style'
+import { createWidget, widget, align, prop, text_style } from '@zos/ui'
 
 const logger = Logger.getLogger('anxietyrelief')
 
@@ -9,6 +10,18 @@ Page({
     logger.debug('page build invoked')
     hmUI.createWidget(hmUI.widget.TEXT, {
       ...TEXT_STYLE,
+    })
+    const text = createWidget(widget.TEXT, {
+      x: 96,
+      y: 120,
+      w: 288,
+      h: 46,
+      color: 0xffffff,
+      text_size: 36,
+      align_h: align.CENTER_H,
+      align_v: align.TOP,
+      text_style: text_style.NONE,
+      text: 'Text Widget'
     })
   },
   onInit() {
