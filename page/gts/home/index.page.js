bcedca8c0dfe19361aeafdf36f8fd11897d5b768
Ziqing (Rezi) Zhang <ziqing.zhang01@sjsu.edu>
2022-10-19 22:01:29 -0700

    Heart Rate Sensor Display
    Displaying heart rate within the app.

--- page/gts/home/index.page.js
+++ page/gts/home/index.page.js
@@ -1,12 +1,29 @@
 import * as hmUI from '@zos/ui'
 import { log as Logger } from '@zos/utils'
 import { TEXT_STYLE } from './index.style'
+import { HeartRate } from '@zos/sensor'
+
+const heartRate = new HeartRate()
+
+const callback = () => {
+  console.log(heartRate).getLast()
+}
+
+calorie.onLastChange(callback)
 
 Page({
   build() {
-    logger.debug('page build invoked')
-    hmUI.createWidget(hmUI.widget.TEXT, {
-      ...TEXT_STYLE,
+    const text = hmUI.createWidget(hmUI.widget.TEXT, {
+      x: px(96),
+      y: px(120),
+      w: px(288),
+      h: px(46),
+      color: 0xffffff,
+      text_size: 36,
+      align_h: hmUI.align.CENTER_H,
+      align_v: hmUI.align.CENTER_V,
+      text_style: hmUI.text_style.NONE,
+      text: "Heart Rate: " + heart.last
     })
   },
   onInit() {
